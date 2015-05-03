303bfc6b08013deb624f185d14e4c3280b573673
Eric Chen <mouthwood@gmail.com>
2015-05-03 12:05:01 -0700

    include mongoose config files for future use

---
+++ config/mongoose.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// var mongoose = require('mongoose'),
+// env = process.env.NODE_ENV || 'development',
+// config = require('./mongoConfig')[env],
+// fs = require('fs');
+
+// // Bootstrap db connection
+// var connect = function() {
+//   var options = {server: {socketOptions: {keepAlive: 1}}};
+//   mongoose.connect(config.db, options);
+// }
+
+// // Connect to mongodb
+// connect();
+
+// // Error handler
+// mongoose.connection.on('error', function(err){
+//   console.log(err);
+// })
+
+// // Reconnect when closed
+// mongoose.connection.on('disconnected', function(){
+//   connect();
+// })
+
+// // Bootstrap models
+// var models_path = __dirname + '/../server/models';
+// fs.readdirSync(models_path).forEach(function(file){
+//   if (~file.indexOf('.js')) require(models_path + '/' + file);
+// })
---
+++ server/models/mongooseModel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// var mongoose = require('mongoose');
+
+// // CREATE SCHEMA
+// var tempSchema = new mongoose.Schema({
+//      user_name: {type: String},
+//      email_address: {type: String},  
+//      password: {type: String},
+//      message: Array,
+//  });
+
+// mongoose.model('temp', tempSchema);
+
