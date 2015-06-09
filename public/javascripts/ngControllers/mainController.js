4750d51d37dcfc2b9775379b0dd94e0b2e143a99
Eric Chen <mouthwood@gmail.com>
2015-06-09 00:35:31 -0700

    display posts in sequence compare to current time.

--- public/javascripts/ngControllers/mainController.js
+++ public/javascripts/ngControllers/mainController.js
@@ -5,11 +5,9 @@
 angular.module('demo').controller('mainCtrl', ['$scope', '$compile', 'mainFty', function($scope, $compile, mainFty) {
     
     this.getSelectTime = mainFty.retrieveSelectTime();
-    this.posts = mainFty.retrievePosts();
-    // this.todayFuturePosts = mainFty.retrieveTodayFuture();
-    // this.todayPastPosts = mainFty.retrieveTodayPast();
+    this.todayFuturePosts = mainFty.retrieveTodayFuture();
+    this.todayPastPosts = mainFty.retrieveTodayPast();
     this.createPost = function() {
-        console.log($scope.message, $scope.timeSelected);
         if($scope.message && $scope.timeSelected) {
             var inputTimeString = Date.parse(
                 $scope.timeSelected.mm +" "+
@@ -18,9 +16,9 @@ angular.module('demo').controller('mainCtrl', ['$scope', '$compile', 'mainFty', 
                 $scope.timeSelected.hour +":"+
                 $scope.timeSelected.minute);
             mainFty.addPost($scope.message, inputTimeString);
+
             $scope.message='';
-            $scope.timeSelected.dd = '';
-            $scope.timeSelected.yyyy = '';
+            $scope.timeSelected=undefined;
         }else if($scope.message) {
             var currTime = Date.now();
             mainFty.addPost($scope.message, currTime);
