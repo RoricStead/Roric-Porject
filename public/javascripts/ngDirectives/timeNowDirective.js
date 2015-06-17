27035d47876254e65039e85c5cae0aed7650988b
Eric Chen <mouthwood@gmail.com>
2015-06-17 00:10:18 -0700

    updateDate function to update the posts after 12:00AM

--- public/javascripts/ngDirectives/timeNowDirective.js
+++ public/javascripts/ngDirectives/timeNowDirective.js
@@ -12,10 +12,13 @@ angular.module('demo').directive('timeNow', ['dateFilter', '$timeout', 'timeFunc
             var updateTime = function() {
                 var now = new Date();
                 element.html(dateFilter(now.valueOf(), format));
-                if(now.getMinutes()%5 === 0 && now.getSeconds() === 0) {
+                if(now.getHours() !== 0 && now.getMinutes()%5 === 0 && now.getSeconds() === 0) {
+                    console.log(now.getHours(), now.getMinutes(), now.getSeconds(), 'every 5 minutes?');
                     mainFty.updatePost(now);
-                } else if(now.getHours() === 23) {
+                 } else if(now.getHours() === 0 && now.getMinutes() === 0) {
+                    console.log(now.getHours(), now.getMinutes(), now.getSeconds(), 'every day?');
                     mainFty.updateDate(now);
+                    mainFty.updatePost(now);
                 }
                 $timeout(updateTime, 1000);
             }
